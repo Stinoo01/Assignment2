390a5d73fba0f23b1ddf61ecf569f91785509dbe
Stinoo01 <93342130+Stinoo01@users.noreply.github.com>
2022-01-20 11:34:32 +0100

    Update prob.py

--- prob.py
+++ prob.py
@@ -1,17 +1,17 @@
-#import logarithm
+#importing 
 import math
 from math import log
 
 #opening file with data 
 f = open('dati.txt', 'r').readlines()
 
-def calcProb(gc, seq):
+def calculus(gc, seq):
     #probbility of GC 
-    gchalf = gc/2.0
-    athalf = (1.0-gc)/2.0
+    gchalf = gc/2
+    athalf = (1-gc)/2
 
     #multiply probability for each letter in the sequence
-    prob = 1.0
+    prob = 1
     #seq = sequence studied 
     for i in range(0, len(seq)):
         if seq[i] == 'A' or seq[i] == 'T':
@@ -25,10 +25,11 @@ def calcProb(gc, seq):
 #take log of calcProb
 #use 3 decimla points 
 gc = f[1].split( )
-final_ = []
-for x in gc:
-    final = calcProb(float(x), f[0])
-    final_.append("%.3f" % math.log10(final))
 
-#print result spaced
-print(" ".join(final_))
+result = []
+for x in gc:
+    final = calculus(float(x), f[0])
+    result.append("%.3f" % math.log10(final)) #3 decimal points
+
+#print result with a space
+print(" ".join(result))
