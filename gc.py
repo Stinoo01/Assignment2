222b29416ad0328df5129915fc0457c058b38713
Stinoo01 <93342130+Stinoo01@users.noreply.github.com>
2022-01-20 11:26:34 +0100

    Update gc.py

--- gc.py
+++ gc.py
@@ -1,49 +1,26 @@
-#GC content of DNA sequence 
-def counterGC(sequence):
-
-    #count G and C
-    def count(base):
-        if base == "G" or base == "C":
-            return 1
-        else:
-            return 0
-
-    #sum G and C
-    GC = sum(map(count, sequence))
-
-    #doing percentage
-    return GC * 100.0 / len(sequence)
-
-#use dictionary with {ID: sequence}
-def fastaformat(fasta):
-    #current ID
-    seq = None
-    #creating dictionary 
-    output = {}
-    #creating fasta and update dictionary
-    for x in fasta:
-        if x.startswith(">"):
-            seq = x.replace(">","")
-            output[seq] = ""
-        else:
-            output[seq] += x
-    # Return fasta dictionary
-    return output
+from Bio import SeqIO
 
 #open file 
-f = open("dati.txt","r").readlines()
+f = open('dati.txt', 'r')
 
-#remove end of line 
-f = list(map(lambda x: x.replace("\n",""), f))
+for sequence in SeqIO.parse(f, 'fasta'):
+    #sequence id = rosalind 
+    ID = sequence.id
+    GC = 0 #storge GC
+    tot = 0 #len sequence 
+    #check if base is G or C and add to count 
+    for base in sequence.seq:
+        #add count for len sequence 
+        tot += 1
+        #add if equal to base 
+        if base == 'G' or base == 'C':
+            GC += 1
+        else:
+            GC += 0
+    
+    #percentage 
+    percentage = GC*100 /tot
 
-#pass to the dictionary
-fasta_dict = fastaformat(f)
-
-#calculate gc
-gc = list(map(lambda x: (x[0], counterGC(x[1])), fasta_dict.items()))
-
-#sort by gc content
-gc = sorted(gc, key=lambda x: -x[1])
-
-#retinging highest value 
-print("{0}\n{1:6f}".format(gc[0][0], gc[0][1]))
+print(ID) #risalind 
+print(percentage) #percentage 
+f.close() #close file 
